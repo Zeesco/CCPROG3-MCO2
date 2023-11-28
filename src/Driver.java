6188cb9979012bd0c00911d6ccdf555e6a41ed20
Yuan Butiong <yuanbutiong@Yuans-MacBook-Air.local>
2023-11-28 18:38:28 +0800

    added comment

--- src/Driver.java
+++ src/Driver.java
@@ -5,6 +5,7 @@ import Controller.MainController;
 
 public class Driver {
     public static void main(String[] args) {
+        // added comment
         MainModel model = new MainModel();
         View view = new View();
         MainController controller = new MainController(model,view);
